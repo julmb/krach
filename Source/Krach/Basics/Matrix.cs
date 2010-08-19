8adcd662f7e38c0c9220299be7d1786fbb56b364
Julian Brunner <julianbrunner@gmail.com>
2010-08-19 22:21:00 +0000

    Added DCT.

--- Source/Krach/Basics/Matrix.cs
+++ Source/Krach/Basics/Matrix.cs
@@ -47,11 +47,16 @@ namespace Krach.Basics
 
 		public Matrix(int rows, int columns)
 		{
-			values = new double[rows, columns];
+			this.values = new double[rows, columns];
 		}
 		public Matrix(double[,] values)
 		{
 			this.values = values;
+		}
+		public Matrix(double[] values)
+			: this(values.Length, 1)
+		{
+			for (int row = 0; row < Rows; row++) this[row, 0] = values[row];
 		}
 
 		public override bool Equals(object obj)
@@ -88,6 +93,16 @@ namespace Krach.Basics
 		public bool Equals(Matrix other)
 		{
 			return this == other;
+		}
+		public double[] ToValues()
+		{
+			if (Columns != 1) throw new InvalidOperationException("Cannot convert non-Vector to values.");
+
+			double[] values = new double[Rows];
+
+			for (int row = 0; row < Rows; row++) values[row] = this[row, 0];
+
+			return values;
 		}
 
 		public Matrix Power(int exponent)
---
+++ Source/Krach/Fourier/DiscreteCosineTransformer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Krach.Extensions;
+using Krach.Basics;
+
+namespace Krach.Fourier
+{
+	public class DiscreteCosineTransformer
+	{
+		public static double[] Transform(double[] values)
+		{
+			return (GetTransformation(values.Length) * new Matrix(values)).ToValues();
+		}
+
+		static Matrix GetTransformation(int size)
+		{
+			Matrix transformation = new Matrix(size, size);
+
+			for (int row = 0; row < transformation.Rows; row++)
+				for (int column = 0; column < transformation.Columns; column++)
+					transformation[row, column] = Scalars.Cosine((Math.PI / size) * (0.5 + column) * row);
+
+			return transformation;
+		}
+	}
+}
