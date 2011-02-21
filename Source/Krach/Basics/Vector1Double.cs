ba9f3f84732676c3683aca8409868875cebf8dc8
Julian Brunner <julianbrunner@gmail.com>
2011-02-21 18:01:04 +0000

    Updated header date.

--- Source/Krach/Basics/Vector1Double.cs
+++ Source/Krach/Basics/Vector1Double.cs
@@ -1,18 +1,19 @@
-// Copyright © Julian Brunner 2010
+// Copyright Â© Julian Brunner 2009 - 2011
 
-// This file is part of Krach.
-//
-// Krach is free software: you can redistribute it and/or modify it under the
-// terms of the GNU Lesser General Public License as published by the Free
+// This file is part of Stream Visualizer (streamvis).
+// 
+// Stream Visualizer is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
 // Software Foundation, either version 3 of the License, or (at your option) any
 // later version.
-//
-// Krach is distributed in the hope that it will be useful, but WITHOUT ANY
-// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
+// 
+// Stream Visualizer is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+// 
 // You should have received a copy of the GNU General Public License along with
-// Krach. If not, see <http://www.gnu.org/licenses/>.
+// Stream Visualizer. If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using Krach.Extensions;
