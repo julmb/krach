c0adec72bcfa81f311b1fb50475db72e91b9d29e
Julian Brunner <julianbrunner@gmail.com>
2013-06-27 18:21:42 +0000

    added collection replace method

--- Source/Krach/Extensions/Collections.cs
+++ Source/Krach/Extensions/Collections.cs
@@ -35,5 +35,13 @@ namespace Krach.Extensions
 
 			foreach (TSource item in items) source.Remove(item);
 		}
+		public static void Replace<TSource>(this ICollection<TSource> source, IEnumerable<TSource> items)
+		{
+			if (source == null) throw new ArgumentNullException("source");
+			if (items == null) throw new ArgumentNullException("items");
+
+			source.Clear();
+			source.Add(items);
+		}
 	}
 }
